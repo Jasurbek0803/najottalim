af09a37dba6300f3f660335a1916886c27b19314
Jasurbek0803 <jasurbekaktamov080@gmail.com>
2023-03-27 16:54:15 +0500

    yangi ozgarish

--- src/main/java/uz/najottalim/Application.java
+++ src/main/java/uz/najottalim/Application.java
@@ -1,2 +1,8 @@
-package uz.najottalim;public class Application {
+package uz.najottalim;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("first ");
+        System.out.println("second");
+    }
 }

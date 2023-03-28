dd91515ed0681e028b249063560b598cdb4ccd67
Jasurbek0803 <jasurbekaktamov080@gmail.com>
2023-03-28 15:00:44 +0500

    new commit

--- src/main/java/uz/najottalim/Application.java
+++ src/main/java/uz/najottalim/Application.java
@@ -4,5 +4,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("first ");
         System.out.println("second");
+        System.out.println("third");
     }
 }
+
+
